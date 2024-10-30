7bb3dd3f3206025ea39d3db424cc10dcc66dd963
moskovtsev_andrei <moskovtsev.af@phystech.edu>
2024-10-30 16:17:26 +0300

    made inference for single channel images, grayscale and binarization transforms were added

--- src/unet_inference.py
+++ src/unet_inference.py
@@ -5,6 +5,9 @@ import argparse
 import time
 from models.model import *
 from data.dataset import *
+import torchvision.transforms as transforms
+from models.model import Generator
+from data.dataset import OPCDataset, TestDataset, BinarizeTransform
 from torch.utils.data import DataLoader
 
 
@@ -19,37 +22,33 @@ device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 print(f'Running inference on device: {device}')
 
 def save_generated_image(output, epoch, step, checkpoint_dir="checkpoints", image_type='true_correction'):
-    # # Convert from [-1, 1] to [0, 255] for saving
-    # output = 0.5 * (output + 1)  # Brings values to [0, 1] range
-
-    single_image = torch.mean(output[0], axis=0)
+    single_image = output[0].squeeze(dim=0)
     single_image[single_image > 0.5] = 1.0
     single_image[single_image <= 0.5] = 0.0
 
     img_save_path = os.path.join(checkpoint_dir, f"{image_type}_epoch{epoch}_step{step}.png")
     cv2.imwrite(f"{img_save_path}", (single_image*255).detach().cpu().numpy())
-    # saved_img = Image.open(img_save_path)
-    # saved_img.show()
     print(f"Saved generated image at {img_save_path}")
 
-generator_model = Generator(in_ch = 3, out_ch = 3)
+generator_model = Generator(in_ch = 1, out_ch = 1)
 # generator_model.load_state_dict(torch.load(MODEL_PATH,map_location=torch.device('cpu')))
 generator_model.load_state_dict(torch.load(MODEL_PATH, map_location = device)['model_state_dict'])
 generator_model = generator_model.to(device)
 generator_model.eval()
 print('Model initialized:', generator_model)
 
-transform = transforms.Compose([
+TRANSFORM = transforms.Compose([
     transforms.Resize((1024, 1024)),
     transforms.ToTensor(),
+    transforms.Grayscale(),
+    BinarizeTransform(threshold=0.5)
 ])
 
 # Dataset paths
-# test_dataset = OPCDataset("data/processed/gds_dataset_new/origin/test_origin", "data/processed/gds_dataset_new/correction/test_correction", transform=transform)
-test_dataset = TestDataset(DATA_PATH, transform=transform)
+TEST_DATASET = TestDataset(DATA_PATH, transform=TRANSFORM)
 
 # DataLoader
-TEST_LOADER = DataLoader(test_dataset, batch_size=1, shuffle=True)
+TEST_LOADER = DataLoader(TEST_DATASET, batch_size=1, shuffle=False)
 
 for idx, image in enumerate(TEST_LOADER):
     start_time = time.time()
@@ -60,8 +59,6 @@ for idx, image in enumerate(TEST_LOADER):
     save_generated_image(output_mask, epoch=1, step=idx, checkpoint_dir=OUTPUT_DIR, image_type='generated_mask')
     end_time = time.time()
     print(f'Performed inference within:{end_time - start_time} seconds')
-
-
 
 # image = cv2.imread(IMAGE_PATH)
 # image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
