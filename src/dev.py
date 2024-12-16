90af09bd05c778dc6361e92de40f43fcad8b0736
moskovtsev_andrei <moskovtsev.af@phystech.edu>
2024-12-16 18:43:48 +0300

    made dev.py refactoring

--- src/dev.py
+++ src/dev.py
@@ -16,8 +16,10 @@ from utils import BoundaryLoss, TVLoss, ContourLoss, IouLoss, PixelAccuracy
 matplotlib.use('Agg')
 
 MODEL_PATH = os.path.join(CHECKPOINT_PATH, 'exp_3/last_checkpoint.pth')
-OUTPUT_DIR = 'inference/output_img'
+OUTPUT_DIR = 'data/external'
 DEVICE = torch.device('cuda:0')
+CHECK_DIMENSIONS = True
+SAVE_PREDICTION = False
 
 generator_model = Generator(in_ch = 1, out_ch = 1)
 # generator_model.load_state_dict(torch.load(MODEL_PATH,map_location=torch.device('cpu')))
@@ -99,48 +101,23 @@ pixel_acc = PixelAccuracy()
 accuracy = pixel_acc(pred, target)
 print(f'Pixel Accuracy: {accuracy}')
 
-# # 1. Find the maximum values across the last two dimensions
-# max_across_width, _ = image.max(dim=(-1))
-# max_values, _ = max_across_width.max(dim=-1)
-# print("Maximum values across last two dimensions:")
-# print(max_values)
-# print("Shape of max values:", max_values.shape)  # Expected shape: [1, 3]
-#
-# # 2. Find unique values across the last two dimensions
-# reshaped_tensor = image.view(image.size(0), image.size(1), -1)  # Shape: [1, 3, 1024*1024]
-# unique_values = torch.unique(reshaped_tensor)
-#
-# print("Unique values across last two dimensions:")
-# print(unique_values)
-# print("Number of unique values:", unique_values.numel())
+if CHECK_DIMENSIONS:
+  # 1. Find the maximum values across the last two dimensions
+  max_across_width, _ = image.max(dim=(-1))
+  max_values, _ = max_across_width.max(dim=-1)
+  print("Maximum values across last two dimensions:")
+  print(max_values)
+  print("Shape of max values:", max_values.shape)  # Expected shape: [1, 3]
 
-# # Выполним проверку подсчёта функций потерь
-# with torch.no_grad():
-#   output = generator_model(image)
-#
-# print(f'Output shape: {output.shape}')
-#
-# boundary_loss = BoundaryLoss(weight=1.0, device = DEVICE)
-# iou_loss_value = iou_loss(target, output.sigmoid())
-# mse_loss_value = torch.nn.functional.mse_loss(output.sigmoid(), target)
-# boundary_loss_value = boundary_loss(output.sigmoid(), target)
-#
-# print(output.shape)
-# print(f'IoU loss:{iou_loss_value}')
-# print(f'L2-loss:{mse_loss_value}')
-# print(f'Boundary loss:{boundary_loss_value}')
-#
-# print(f'L2-loss shape:{mse_loss_value.shape}')
-# print(f'IoU-loss shape:{iou_loss_value.shape}')
-# #print(f'Boundary loss shape:{boundary_loss.shape}')
-#
-# total_loss = mse_loss_value + iou_loss_value + boundary_loss_value
-# total_loss.backward()
+  # 2. Find unique values across the last two dimensions
+  reshaped_tensor = image.view(image.size(0), image.size(1), -1)  # Shape: [1, 3, 1024*1024]
+  unique_values = torch.unique(reshaped_tensor)
 
-#save_generated_image(output.sigmoid(), epoch=0, step=0, checkpoint_dir=OUTPUT_DIR, image_type='generated_test')
+  print("Unique values across last two dimensions:")
+  print(unique_values)
+  print("Number of unique values:", unique_values.numel())
 
-# for idx, (image, target) in enumerate(TEST_LOADER):
-#   with torch.no_grad():
-#     output = generator_model(image)
-#     save_generated_image(output.sigmoid(), epoch=1, step=0, checkpoint_dir=OUTPUT_DIR, image_type=f'generated_mask_{idx}')
+if SAVE_PREDICTION:
+  save_generated_image(pred, epoch=0, step=0, checkpoint_dir=OUTPUT_DIR, image_type='generated_test')
+
 
