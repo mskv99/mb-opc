918dc5f56eb462c53b4e6a9ffcb2b752314d00ae
moskovtsev_andrei <moskovtsev.af@phystech.edu>
2024-12-24 13:44:07 +0300

    added new method - evaluate_model to perform evaluation on test and valid sets

--- src/evaluate.py
+++ src/evaluate.py
@@ -2,11 +2,16 @@ import torch
 import random
 from PIL import Image
 import torchvision.transforms as transforms
+import logging
+from tqdm import tqdm
 import numpy as np
 import os
 
-from dataset import OPCDataset, TestDataset, BinarizeTransform
+from dataset import OPCDataset, TestDataset, BinarizeTransform, apply_transform
 from torch.utils.data import DataLoader, Dataset
+from config import DATASET_PATH, CHECKPOINT_PATH
+from models.unet import Generator
+from utils import IoU, PixelAccuracy
 
 # fixing seeds during evaluation
 def set_random_seed(seed):
@@ -18,29 +23,6 @@ def set_random_seed(seed):
   random.seed(seed)
 
 set_random_seed(42)
-
-class EvalDataset(Dataset):
-  def __init__(self, target_dir, mask_dir, transform=None):
-    self.target_dir = target_dir
-    self.mask_dir = mask_dir
-    self.transform = transform
-    self.images = os.listdir(target_dir)
-
-  def __len__(self):
-    return len(self.images)
-
-  def __getitem__(self, index):
-    target_path = os.path.join(self.target_dir, self.images[index])
-    mask_path = os.path.join(self.mask_dir, self.images[index])
-
-    target = Image.open(target_path)
-    mask = Image.open(mask_path)
-
-    if self.transform:
-      target = self.transform(target)
-      mask = self.transform(mask)
-
-    return target, mask
 
 def calculate_iou(eval_loader):
   iou_list = []
@@ -68,15 +50,63 @@ TRANSFORM = transforms.Compose([
   BinarizeTransform(threshold=0.5)
 ])
 
-TARGET_PATH = 'data/processed/gds_dataset/correction/test_correction'
-MASK_PATH = 'inference/output_img/exp_6'
-BATCH_SIZE = 2
+def evaluate_model(model, loader, device='cuda', log=False):
+  model.eval()
 
-# Dataset paths
-EVAL_DATASET = EvalDataset(TARGET_PATH, MASK_PATH, transform = TRANSFORM)
+  pixel_acc_epoch = 0
+  iou_epoch = 0
 
-# DataLoader
-EVAL_LOADER = DataLoader(EVAL_DATASET, batch_size = BATCH_SIZE, shuffle = False)
+  with torch.no_grad():
+    for idx, (image, target) in tqdm(enumerate(loader)):
+      image, target = image.to(device), target.to(device)
+      params = model(image)
+      mask = torch.sigmoid(params)
 
-calculate_iou(EVAL_LOADER)
-calculate_pixel_accuracy(EVAL_LOADER)
+      # calculating metrics for evaluation
+      pixel_acc_iter = pixel_accuracy(mask, target)
+      iou_iter = iou(mask, target)
+
+      pixel_acc_epoch += pixel_acc_iter.item()
+      iou_epoch += iou_iter.item()
+
+    log_info = {
+      'pixel_acc': pixel_acc_epoch,
+      'iou': iou_epoch,
+      'len_loader': len(loader)
+    }
+
+    # Print and log the message
+  print(f"Pixel Accuracy: {log_info['pixel_acc'] / log_info['len_loader'] :.4f}, ")
+  print(f"IoU: {log_info['iou'] / log_info['len_loader'] :.4f}")
+
+  if log:
+    logging.info(f"Pixel Accuracy: {log_info['pixel_acc'] / log_info['len_loader'] :.4f}, ")
+    logging.info(f"IoU: {log_info['iou'] / log_info['len_loader'] :.4f}")
+
+
+
+  return log_info['pixel_acc'] / log_info['len_loader'], log_info['iou'] / log_info['len_loader']
+
+if __name__ == '__main__':
+  MODEL_PATH = os.path.join(CHECKPOINT_PATH, 'exp_9/last_checkpoint.pth')
+  BATCH_SIZE = 2
+  DEVICE = 'cuda'
+
+  TRAIN_DATASET = OPCDataset(os.path.join(DATASET_PATH, 'origin/train_origin'), os.path.join(DATASET_PATH,'correction/train_correction'), transform = apply_transform(binarize_flag = True))
+  VALID_DATASET = OPCDataset(os.path.join(DATASET_PATH, 'origin/valid_origin'), os.path.join(DATASET_PATH, 'correction/valid_correction'), transform = apply_transform(binarize_flag = True))
+  TEST_DATASET = OPCDataset(os.path.join(DATASET_PATH, 'origin/test_origin'), os.path.join(DATASET_PATH, 'correction/test_correction'), transform = apply_transform(binarize_flag = True))
+
+  # Define dataloader
+  TRAIN_LOADER = DataLoader(TRAIN_DATASET, batch_size = BATCH_SIZE, shuffle = True, num_workers = 2)
+  VALID_LOADER = DataLoader(VALID_DATASET, batch_size = BATCH_SIZE, shuffle = False, num_workers = 2)
+  TEST_LOADER = DataLoader(TEST_DATASET, batch_size = BATCH_SIZE, shuffle = False, num_workers = 2)
+
+  generator_model = Generator(in_ch = 1, out_ch = 1)
+  generator_model.load_state_dict(torch.load(MODEL_PATH, map_location = DEVICE)['model_state_dict'])
+  generator_model = generator_model.to(DEVICE)
+  pixel_accuracy = PixelAccuracy()
+  iou = IoU()
+  # just in case we want to store the output in txt file and
+  # test the function implementation
+  logging.basicConfig(filename = 'log.txt',datefmt = '%d/%m/%Y %H:%M')
+  evaluate_model(model = generator_model, loader=TEST_LOADER, device=DEVICE, log=False)
