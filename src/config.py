f40a9e959f3422e7df91ba00a9daafc35a75223c
moskovtsev_andrei <moskovtsev.af@phystech.edu>
2025-01-13 18:00:44 +0300

    added a path to classification dataset to config

--- src/config.py
+++ src/config.py
@@ -3,9 +3,11 @@ import os
 if os.getenv('ENV') == 'TRAIN_MACHINE':
   DATASET_PATH = '/home/amoskovtsev/projects/mb_opc/data/processed/gds_dataset'
   CHECKPOINT_PATH = '/mnt/data/amoskovtsev/mb_opc/checkpoints'
+  CLASSIFACTION_DATA_PATH = '/home/amoskovtsev/projects/mb_opc/data/processed/gds_dataset/classifier'
 
 else:
   DATASET_PATH = '/workarea/otdMDP/users/amoskovtsev/Pycharm_proj/MB_OPC/custom_unet/data/processed/gds_dataset'
+  CLASSIFACTION_DATA_PATH = '/workarea/otdMDP/users/amoskovtsev/Pycharm_proj/MB_OPC/custom_unet/data/processed/classifier'
   CHECKPOINT_PATH = 'checkpoints'
 
 BATCH_SIZE = 3
