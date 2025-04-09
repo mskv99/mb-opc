cc4c54f664453464947a27715f2ffff1976e593b
moskovtsev_andrei <moskovtsev.af@phystech.edu>
2025-04-09 18:17:15 +0300

    changing configs for hydra

--- src/train.py
+++ src/train.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import torch
 import hydra
 from hydra.utils import instantiate
 from omegaconf import OmegaConf, DictConfig
@@ -16,20 +17,36 @@ def train(cfg: DictConfig):
   print(cfg['model'])
   print(cfg['optim'])
   print(cfg['sched'])
+
   scheduler = instantiate(cfg['sched'])
   optimizer = instantiate(cfg['optim'])
   dataset_path = cfg['env']['paths']['dataset']
   checkpoint_dir = cfg['env']['paths']['checkpoint']
   batch_size = cfg['batch_size']
+  epochs = cfg['epochs']
 
-  losses = {k: instantiate(v) for k, v in cfg['loss'].items()}
+  losses = [instantiate(v) for k, v in cfg['loss'].items() if k != 'weights']
+  loss_names = cfg['loss']['weights'].keys()
+  losses_dict = dict(zip(loss_names, tuple(zip(losses[0], cfg['loss']['weights'].values()))))
+  weights_dict = cfg['loss']['weights']
+
   print(f'Scheduler: {scheduler}')
   print(f'Optimizer: {optimizer}')
   print(f'Dataset path: {dataset_path}')
   print(f'Checkpoint dir: {checkpoint_dir}')
   print(f'Batch size: {batch_size}')
   print(f'Model: {model}')
-  print(f'Losses: {losses}')
+  print(f'Epochs: {epochs}')
+
+  print(f'losses: {losses}')
+  print(f'loss_names: {loss_names}')
+  print(f'losses_dict: {losses_dict}')
+  print(f'weights: {weights_dict}')
+
+  # iou_loss = losses['loss']
+  prediction = torch.randn((1, 1, 30, 30), dtype=torch.float32).sigmoid()
+  target = torch.ones((1, 1, 30, 30), dtype=torch.float32)
+  # print(f'iou_loss: {iou_loss(prediction, target)}')
 
 
 if __name__ == '__main__':
