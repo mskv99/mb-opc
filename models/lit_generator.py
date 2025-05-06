b8273b726373021da83af675897cd9bea61a69d7
moskovtsev andrei <moskovtsev.af@phystech.edu>
2025-05-06 22:12:12 +0300

    corrected test step in LitGenerator(lightning module)

--- models/lit_generator.py
+++ models/lit_generator.py
@@ -9,7 +9,7 @@ from src.metrics import IoU, PixelAccuracy
 
 
 class LitGenerator(pl.LightningModule):
-    def __init__(self, config, log_dir):
+    def __init__(self, config):
         super().__init__()
         self.config = config
         self.save_hyperparameters(config)
@@ -17,20 +17,20 @@ class LitGenerator(pl.LightningModule):
         self.criterion = instantiate(config["loss"])
         self.iou = IoU()
         self.pixel_acc = PixelAccuracy()
-        self.val_example_logged = False
-        self.log_dir = log_dir
-        self.test_step_outputs = []
+        self.val_example_logged = False # flag for logging images during validation phase
 
     def forward(self, x):
         return self.model(x)
 
     def compute_loss(self, pred, target):
+        # compute the sum of loss components for optimization
+        # store different loss components in dict for logging
         losses = self.criterion(pred, target)
         if isinstance(losses, dict):
             total_loss = sum(losses.values())
             return total_loss, losses
         else:
-            return losses, {"loss": losses}  # handle non-dict criterion
+            return losses, {"loss": losses}
 
     def training_step(self, batch, batch_idx):
         origin, target = batch
@@ -39,7 +39,7 @@ class LitGenerator(pl.LightningModule):
         iou = self.iou(pred, target)
         pixel_acc = self.pixel_acc(pred, target)
 
-        # log all loss components
+        # separate logging for all loss components
         for k, v in loss_dict.items():
             self.log(
                 f"train/lossG_{k}/epoch", v, prog_bar=True, on_step=False, on_epoch=True
@@ -55,15 +55,19 @@ class LitGenerator(pl.LightningModule):
         loss, loss_dict = self.compute_loss(pred, target)
         iou = self.iou(pred, target)
         pixel_acc = self.pixel_acc(pred, target)
-        # Save first prediction on valid data
+        # save first prediction on valid data
         if not self.val_example_logged and batch_idx == 0:
             save_image(
                 pred,
-                os.path.join(self.log_dir, f"pred_epoch{self.current_epoch}.png"),
+                os.path.join(
+                    self.logger.log_dir, f"pred_epoch{self.current_epoch}.png"
+                ),
             )
 
             if isinstance(self.logger, pl.loggers.WandbLogger):
-                self.logger.experiment.log({f"val/sample_epoch{self.current_epoch}": wandb.Image(pred[0])})
+                self.logger.experiment.log(
+                    {f"val/sample_epoch{self.current_epoch}": wandb.Image(pred[0])}
+                )
 
             self.val_example_logged = True
 
@@ -82,38 +86,26 @@ class LitGenerator(pl.LightningModule):
         loss, loss_dict = self.compute_loss(pred, target)
         iou = self.iou(pred, target)
         pixel_acc = self.pixel_acc(pred, target)
-        self.test_step_outputs.append({"iou": iou, "pixel_acc": pixel_acc})
-        return {
-            "iou": iou,
-            "pixel_acc": pixel_acc,
-        }
+        for k, v in loss_dict.items():
+            self.log(
+                f"test/lossG_{k}/epoch", v, prog_bar=True, on_step=False, on_epoch=True
+            )
+        self.log(f"test/pixel_acc/epoch", pixel_acc, on_step=False, on_epoch=True)
+        self.log(f"test/iou/epoch", iou, on_step=False, on_epoch=True)
+
+        return loss
 
     def on_validation_start(self):
         self.val_example_logged = False
 
-    def on_train_epoch_end(self):
-        pass
-
-    def on_validation_epoch_end(self):
-        pass
-
-    def on_test_epoch_end(self) -> None:
-        ious = torch.stack([o["iou"] for o in self.test_step_outputs])
-        pixel_accs = torch.stack([o["pixel_acc"] for o in self.test_step_outputs])
-        avg_iou = ious.mean().item()
-        avg_pixel_acc = pixel_accs.mean().item()
-
-        return {"iou": avg_iou, "pixel_accuracy": avg_pixel_acc}
-
     def configure_optimizers(self):
-        # 1. Instantiate optimizer (complete the partial)
+        # instantiate optimizer (complete the partial)
         optimizer_fn = instantiate(self.config["optim"])
         optimizer = optimizer_fn(self.parameters())
-        # 2. Instantiate scheduler (complete the partial)
+        # instantiate scheduler (complete the partial)
         scheduler_fn = instantiate(self.config["sched"])
         scheduler = scheduler_fn(optimizer=optimizer)
 
-        # 3. Return in Lightning's expected format
         return {
             "optimizer": optimizer,
             "lr_scheduler": {
